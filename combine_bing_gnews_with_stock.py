0ced3df13e225e88a92e1d762025e8a960b12d48
yixinxiang <37011596+yixinxiang@users.noreply.github.com>
2019-07-23 02:46:35 -0400

    updated the complete data set

--- combine_bing_gnews_with_stock.py
+++ combine_bing_gnews_with_stock.py
@@ -5,8 +5,10 @@ with open('bing_news_data_IT_with_stock.json') as json_file:
 with open('gnews_data_Mar_Apr_IT_with_stock.json') as json_file:
     gnews_data = json.load(json_file)
 
-combined_data = bing_data
-combined_data.append(gnews_data)
+print("bing: " + str(len(bing_data)))
+print("gnews: " + str(len(gnews_data)))
+combined_data = bing_data + gnews_data
+print("total: " + str(len(combined_data)))
 json_data = json.dumps(combined_data)
 
 with open('combined_data_IT_with_stock.json', 'w') as f:
